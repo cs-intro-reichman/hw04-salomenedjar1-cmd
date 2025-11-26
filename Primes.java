6e4e1f5187dc0e5ee5a1560147223faba7a27d17
Salome Nedjar <salomenedjar1@gmail.com>
2025-11-26 15:37:52 +0200

    Completed assignment

--- Primes.java
+++ Primes.java
@@ -25,7 +25,21 @@ public class Primes {
 
             current++;
         }
+        System.out.println("Prime numbers up to " + n + ":");
 
+        int count = 0;
+        for (int i = 2; i <= n; i++) 
+        {
+            if (isPrime[i]) 
+            {
+                System.out.println(i);
+                count++;
+            }
+        }
 
+        int percentage = (int) Math.round((count * 100.0) / n);
+
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + percentage + "% are primes)");
     }
+
 }
